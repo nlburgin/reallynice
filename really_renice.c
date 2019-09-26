964088881b39a46f41f67cf528d08fa914eac287
nlburgin <30382228+nlburgin@users.noreply.github.com>
2019-09-26 12:05:40 -0400

    Add support for multiple PIDs instead of just one
    by accepting this type of argument list, we get closer to parity with standard `renice` command

--- really_renice.c
+++ really_renice.c
@@ -5,30 +5,46 @@
 #include <errno.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <string.h>
 
 
-#define usage "Usage: really_renice nicelevel pid"
+#define usage "Usage: really_renice nicelevel pid..."
+
+
 
 char buffer[512];
 
 int main(int argc, char **argv){
-  if (argc < 3){
-    stdout = stderr;
-    puts(usage);
-    return 1;
-  }
-  
-  const uid_t prev_user = getuid();
-  const gid_t prev_group = getgid();
   setuid(0);	
-  setgid(0);
+	setgid(0);
   
-  char *Nice = *++argv;
-  long pid = atol(*++argv);
-  sprintf(buffer,"/proc/%li/autogroup",pid);
-  FILE *Seriously = fopen(buffer,"w");
-  fputs(Nice,Seriously);
-  fclose(Seriously);
-  setpriority(PRIO_PROCESS,(id_t)pid,atoi(Nice));
-  return errno;
+  stdout = stderr;
+  
+  int status = 0;
+  
+  if (argc < 3){
+    puts(usage);
+    return EINVAL;
+  }
+  char *Nice = argv[1];
+  long pid;
+  FILE *Seriously;
+#define errcheck if (errno != 0){ status = errno; printf("Failed for PID %li:%s\n",pid,strerror(status)); continue; }
+
+  for (int i = 2; i < argc; i++){
+    errno = 0;
+    pid = atol(argv[i]);
+    errcheck
+    sprintf(buffer,"/proc/%li/autogroup",pid);
+    errcheck
+    Seriously = fopen(buffer,"w");
+    errcheck
+    fputs(Nice,Seriously);
+    errcheck
+    fclose(Seriously);
+    errcheck
+    setpriority(PRIO_PROCESS,(id_t)pid,atoi(Nice));
+    errcheck
+  }
+  return status;
 }
